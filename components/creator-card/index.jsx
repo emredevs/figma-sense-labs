abea02136687ba6a776746af3b52f8cef38fd3fe
Emre ER <emre34er@gmail.com>
2023-09-06 18:04:47 +0300

    creator components i tamamlandı

--- components/creator-card/index.jsx
+++ components/creator-card/index.jsx
@@ -1,5 +1,92 @@
 import React from "react";
-import stles from "./stles.module.css";
+import styles from "./stles.module.css";
+import Image from "next/image";
 export default function CreatorCard() {
-  return <div>CreatorCard</div>;
+  return (
+    <div className={styles.creatorContainer}>
+      <h1>Creator’s Other Projects</h1>
+      <div className={styles.creatorBox}>
+        <div className={styles.creatorCard}>
+          <Image src={"/creatorCard.png"} alt="card" width={221} height={221} />
+          <h3>
+            The Big Mac
+            <span> 3 in stock</span>
+          </h3>
+          <span>
+            Creator .{" "}
+            <span>
+              <b>Roger Waters</b>
+            </span>
+          </span>
+
+          <div className={styles.creatorBtnBox}>
+            <button>Mint</button>
+            <div>
+              1.5 <Image src={"/eth.png"} alt="eht" width={16} height={16} />
+            </div>
+          </div>
+        </div>
+        <div className={styles.creatorCard}>
+          <Image src={"/creatorCard.png"} alt="card" width={221} height={221} />
+          <h3>
+            The Big Mac
+            <span> 3 in stock</span>
+          </h3>
+          <span>
+            Creator .{" "}
+            <span>
+              <b>Roger Waters</b>
+            </span>
+          </span>
+
+          <div className={styles.creatorBtnBox}>
+            <button>Mint</button>
+            <div>
+              1.5 <Image src={"/eth.png"} alt="eht" width={16} height={16} />
+            </div>
+          </div>
+        </div>
+        <div className={styles.creatorCard}>
+          <Image src={"/creatorCard.png"} alt="card" width={221} height={221} />
+          <h3>
+            The Big Mac
+            <span> 3 in stock</span>
+          </h3>
+          <span>
+            Creator .{" "}
+            <span>
+              <b>Roger Waters</b>
+            </span>
+          </span>
+
+          <div className={styles.creatorBtnBox}>
+            <button>Mint</button>
+            <div>
+              1.5 <Image src={"/eth.png"} alt="eht" width={16} height={16} />
+            </div>
+          </div>
+        </div>
+        <div className={styles.creatorCard}>
+          <Image src={"/creatorCard.png"} alt="card" width={221} height={221} />
+          <h3>
+            The Big Mac
+            <span> 3 in stock</span>
+          </h3>
+          <span>
+            Creator .{" "}
+            <span>
+              <b>Roger Waters</b>
+            </span>
+          </span>
+
+          <div className={styles.creatorBtnBox}>
+            <button>Mint</button>
+            <div>
+              1.5 <Image src={"/eth.png"} alt="eht" width={16} height={16} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
